acb03cfa13965997aeca30e67c3e528beba93ebc
Aleksandr Gorovoi <=>
2016-05-08 14:30:26 +0300

    [example] Example of use added

---
+++ console_example/src/main/java/ru/vsu/cs/documentpreparing/console_example/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.vsu.cs.documentpreparing.console_example;
+
+/**
+ *
+ * @author aleksandr
+ */
+public class Main {
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args){
+        System.out.print("Hello world!");
+    }
+}
